c09a6b0923dcb867428328b574969969f28d523f
artyomrodin712l <55128809+artyomrodin712l@users.noreply.github.com>
2020-10-30 20:53:11 +0300

    Add buttons

--- src/App.js
+++ src/App.js
@@ -10,31 +10,131 @@ class App extends Component {
       rows: 2,
       columns: 2,
       tableRowData: [],
+      headers: [],
+      isRandom: false,
     };
     this.handleRowsChange = this.handleRowsChange.bind(this);
     this.handleColumnsChange = this.handleColumnsChange.bind(this);
+    this.handleGetRandomInt = this.handleGetRandomInt.bind(this);
+    this.handleCalculate = this.handleCalculate.bind(this);
   }
+
   createMass(n, m) {
-    var tableRowData = []
-    var tableColumnData = []
-    for (var i = 0; i < m; i++) {
-      tableRowData[i] = new Map();
-      for (var j = 0; j < n; j++) {
-        tableColumnData[j] = 0
+    var tableRowData = [];
+    var headers = [];
+    for (var i = 0; i < n; i++) {
+      tableRowData[i] = [];
+      for (var j = 0; j < m; j++) {
+        headers[j] = "x^" + j;
+        tableRowData[i][j] = 0;
       }
-      tableRowData[i].set("rowData", tableColumnData)
     }
-    console.log(tableRowData)
+    console.log(headers);
     this.setState({
       tableRowData: tableRowData,
+      headers: headers,
     });
   }
+  handleCalculate() {
+    var M;
+    var Xk = [];
+    var Zk = [];
+    var Rk = [];
+    var Sz = [];
+    var alpha, beta, mf;
+    var Spr, Spr1, Spz;
+    var A = [];
+    var F = [];
+    var i,
+      j,
+      kl = 1;
+    var max_iter = 100000;
+    var E = 0.001;
+    /* Вычисляем сумму квадратов элементов вектора F*/
+    for (mf = 0, i = 0; i < M; i++) {
+      mf += F[i] * F[i];
+    }
+
+    /* Задаем начальное приближение корней. В Хk хранятся значения корней
+     * к-й итерации. */
+    for (i = 0; i < M; i++) {
+      Xk[i] = 0.2;
+    }
+
+    /* Задаем начальное значение r0 и z0. */
+    for (i = 0; i < M; i++) {
+      for (Sz[i] = 0, j = 0; j < M; j++) Sz[i] += A[i][j] * Xk[j];
+      Rk[i] = F[i] - Sz[i];
+      Zk[i] = Rk[i];
+    }
+    var Iteration = 0;
+    do {
+      Iteration++;
+      /* Вычисляем числитель и знаменатель для коэффициента
+       * alpha = (rk-1,rk-1)/(Azk-1,zk-1) */
+      Spz = 0;
+      Spr = 0;
+      for (i = 0; i < M; i++) {
+        for (Sz[i] = 0, j = 0; j < M; j++) {
+          Sz[i] += A[i][j] * Zk[j];
+        }
+        Spz += Sz[i] * Zk[i];
+        Spr += Rk[i] * Rk[i];
+      }
+      alpha = Spr / Spz; /*  alpha    */
+
+      /* Вычисляем вектор решения: xk = xk-1+ alpha * zk-1, 
+   вектор невязки: rk = rk-1 - alpha * A * zk-1 и числитель для betaa равный (rk,rk) */
+      Spr1 = 0;
+      for (i = 0; i < M; i++) {
+        Xk[i] += alpha * Zk[i];
+        Rk[i] -= alpha * Sz[i];
+        Spr1 += Rk[i] * Rk[i];
+        //cout << "Iter #" << kl;
+        //cout << " " << "X[" << i << "] = " << Xk[i] << endl;
+      }
+      //cout << endl;
+      kl++;
+
+      /* Вычисляем  beta  */
+      beta = Spr1 / Spr;
+
+      /* Вычисляем вектор спуска: zk = rk+ beta * zk-1 */
+      for (i = 0; i < M; i++) Zk[i] = Rk[i] + beta * Zk[i];
+    } while (
+      /* Проверяем условие выхода из итерационного цикла  */
+      Spr1 / mf > E * E &&
+      Iteration < max_iter
+    );
+
+    return 0;
+  }
+
+  handleGetRandomInt() {
+    var n = this.state.rows;
+    var m = this.state.columns;
+    var tableRowData = [];
+    for (var i = 0; i < n; i++) {
+      tableRowData[i] = [];
+      for (var j = 0; j < m; j++) {
+        tableRowData[i][j] = Math.floor(Math.random() * Math.floor(100));
+      }
+    }
+    this.setState({
+      tableRowData: tableRowData,
+      isRandom: true,
+    });
+    console.log(tableRowData);
+    console.log(this.state.isRandom);
+    console.log(this.state.tableRowData);
+  }
+
   handleRowsChange(rows) {
     this.setState({
       rows: rows,
     });
     console.log(this.state.rows);
-    this.createMass(this.state.rows,this.state.columns)
+    this.createMass(this.state.rows, this.state.columns);
   }
 
   handleColumnsChange(columns) {
@@ -42,14 +142,15 @@ class App extends Component {
       columns: columns,
     });
     console.log(this.state.columns);
-    this.createMass(this.state.rows,this.state.columns)
+    this.createMass(this.state.rows, this.state.columns);
   }
-  componentDidMount() {
- 
+  componentDidUpdate() {
+    console.log(this.state.isRandom);
+    if (this.state.isRandom === false) {
+      this.createMass(this.state.rows, this.state.columns);
+    }
   }
   render() {
-    
-    const headers = ["1", "2"];
     return (
       <React.Fragment>
         <InputData
@@ -58,7 +159,9 @@ class App extends Component {
           onRowsChange={this.handleRowsChange}
           onColumnsChange={this.handleColumnsChange}
           tableRowData={this.state.tableRowData}
-          headers={headers}
+          headers={this.state.headers}
+          onClickGetRandom={this.handleGetRandomInt}
+          onClickCalculate={this.handleCalculate}
         />
       </React.Fragment>
     );
--- src/InputData.js
+++ src/InputData.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { Input, Label, FlexBox } from "@ui5/webcomponents-react";
-import { Shellbar, InfoLabel, Table } from "fundamental-react";
+import { Input, Label, FlexBox, Button } from "@ui5/webcomponents-react";
+import { Shellbar, InfoLabel, Table, FormInput } from "fundamental-react";
 
 class InputData extends React.Component {
   constructor(props) {
     super(props);
     this.handleRowsChange = this.handleRowsChange.bind(this);
     this.handleColumnsChange = this.handleColumnsChange.bind(this);
+    this.handleGetRandomInt = this.handleGetRandomInt.bind(this);
+    this.handleCalculate = this.handleCalculate.bind(this);
+  }
+  handleCalculate(){
+      this.props.onClickCalculate()
   }
   handleRowsChange(e) {
     this.props.onRowsChange(e.target.value);
@@ -14,7 +19,30 @@ class InputData extends React.Component {
   handleColumnsChange(e) {
     this.props.onColumnsChange(e.target.value);
   }
+  handleGetRandomInt() {
+    this.props.onClickGetRandom()
+  }
   render() {
+    var tableRowData = this.props.tableRowData;
+    var tableData = tableRowData.map((item, j) => {
+      var rowData = [];
+
+      item.map((itemRowData, i) => {
+        console.log(i);
+        rowData.push(
+          <FormInput
+            aria-label={i + j}
+            defaultValue={itemRowData}
+            name={i + j}
+          />
+        );
+      });
+      return {
+        rowData: rowData,
+      };
+    });
+    console.log(tableRowData);
+    console.log(tableData);
     return (
       <React.Fragment>
         {console.log(this.props.tableData)}
@@ -52,13 +80,50 @@ class InputData extends React.Component {
             value={this.props.columns}
             onChange={this.handleColumnsChange}
           />
+          <FlexBox
+            alignItems="Start"
+            direction="Row"
+            displayInline={false}
+            justifyContent="Start"
+            wrap="NoWrap"
+          >
+            <Button
+              className=""
+              design="Default"
+              disabled={false}
+              icon="employee"
+              iconEnd={false}
+              onClick={this.handleGetRandomInt}
+              slot=""
+              style={{}}
+              submits={false}
+              tooltip=""
+            >
+              Random
+            </Button>
+            <Button
+              className=""
+              design="Positive"
+              disabled={false}
+              icon="employee"
+              iconEnd={false}
+              onClick={this.handleCalculate}
+              slot=""
+              style={{}}
+              submits={false}
+              tooltip=""
+            >
+             Calculate
+            </Button>
+          </FlexBox>
         </FlexBox>
+
         {console.log(this.props.headers)}
         <Table
           compact={true}
           condensed={true}
           headers={this.props.headers}
-          tableData={this.props.tableRowData}
+          tableData={tableData}
         />
       </React.Fragment>
     );
